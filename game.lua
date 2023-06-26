c1fdf07baadcee1ba2188084633117a42900d493
actuallyasmartname <77011692+actuallyasmartname@users.noreply.github.com>
2023-06-26 11:29:50 -0400

    Improve game script

--- game.lua
+++ game.lua
@@ -1,52 +1,47 @@
-game.Players.PlayerAdded:Connect(function(p)
-	local forever = false
-	local initial = true
-	repeat
-		local http = game:GetService("HttpService")
-		local num = 0
-		local freebadge = {}
-		local playerbadge = {}
-		local c = ""
-		local stop = false
-		while stop == false do
-			local s, m = pcall(function()
-				return http:JSONDecode(http:GetAsync("https://badges.roproxy.com/v1/universes/"..game.GameId.."/badges?limit=100&cursor=" .. c .. "&sortOrder=Desc"))
-			end)
-			if type(m) ~= "table" then
-				if initial == true then
-					p:Kick("whoops! something went wrong. check back later")
-				end
-				task.wait(120)
+local initial = true
+while true do
+	local http = game:GetService("HttpService")
+	local num = 0
+	local freebadge = {}
+	local c = ""
+	local stop = false
+	while stop == false do
+		local s, m = pcall(function()
+			return http:JSONDecode(http:GetAsync("https://badges.roproxy.com/v1/universes/"..game.GameId.."/badges?limit=100&cursor=" .. c .. "&sortOrder=Desc"))
+		end)
+		if type(m) ~= "table" then
+			if initial == true then
+				warn("Something went wrong setting up automatic badges. Try rejoining or waiting another time.")
+				do return end
 			end
-			if type(m) == "table" then
-				for i=1,#m.data do
-						table.insert(freebadge,m.data[i].id)
+		elseif type(m) == "table" then
+			for i=1,#m.data do
+				table.insert(freebadge,m.data[i].id)
+			end
+			num += #m.data
+			if m.nextPageCursor == nil then
+				stop = true
+				initial = false
+				local pos = -47.75
+				local pos2 = -47.75
+				for i,v in pairs(game.Workspace:GetDescendants()) do
+					if v.Name == "BadgeAwarder" then
+						v:Destroy()
+					end
 				end
-				num += #m.data
-				if m.nextPageCursor == nil then
-					stop = true
-					initial = false
-					local pos = -47.75
-					local pos2 = -47.75
-					for i,v in pairs(game.Workspace:GetDescendants()) do
-						if v.Name == "BadgePlatform" then
-							v:Destroy()
-						end
-					end
-					local freefolder = Instance.new("Folder", game.Workspace)
-					freefolder.Name = "FreeBadges"
-					for f=1, #freebadge do
-						local clone = game.ReplicatedStorage.BadgePlatform:Clone()
-						clone.Parent = freefolder
-						clone.BadgeID.Value = freebadge[f]
-						clone.Position = Vector3.new(pos-8, -0.5, 5)
-						pos = pos - 8
-					end
+				local freefolder = Instance.new("Folder", game.Workspace)
+				freefolder.Name = "AutomaticBadges"
+				for f=1, #freebadge do
+					local clone = game.ReplicatedStorage.BadgePlatform:Clone()
+					clone.Parent = freefolder
+					clone.BadgeID.Value = freebadge[f]
+					clone.Position = Vector3.new(pos-8, -0.5, 5)
+					pos = pos - 8
+				end
 			else
-				c = m.nextPageCursor
-			end
+			c = m.nextPageCursor
 		end
 	end
-		task.wait(120)
-	until forever == true
-end)
+end
+	task.wait(120)
+end
