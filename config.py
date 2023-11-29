f2b78b5763caa35d5f1ba2b16cf6ced7a4dbb73f
actuallyasmartname <77011692+actuallyasmartname@users.noreply.github.com>
2023-11-29 15:15:36 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ cookie = ""
 universeid = [1] # put in order like this [1,2]
 badgename = "a"
 badgedescription = "a"
-time = 5 # time between making each badge
+timediff = 5 # time between making each badge
 
 valuable = False
 limit = 5 # only sets when valuable is set to True
